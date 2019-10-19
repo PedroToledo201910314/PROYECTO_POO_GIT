4a13afa86e7b7b990d8c8eab7ac40a493b4aeedb
Pedro Alfredo Toledo Garcia <55290916+PedroToledo201910314@users.noreply.github.com>
2019-10-19 16:04:33 -0500

    Update funciones.h

--- funciones.h
+++ funciones.h
@@ -1,13 +1,30 @@
-#ifndef FUNCIONES_H
-#define FUNCIONES_H
-
-#include <iostream>
+#ifndef PROYECTO_H
+#define PROYECTO_H
+#include <vector>
+#include <string>
 using namespace std;
+using numero = int;
+using letra = char;
+using arreglo=vector<char>
 
 class robot {
   private:
   
   public:
   
-  
+
+class almacen{
+  private:
+  numero filas;
+  numero columnas;
+  numero capacidad;
+  vector <arreglo> matriz;
+  public:
+  almacen();
+  almacen(int &_filas, int &_columnas, int &_capacidad, vector <arreglo> &_matriz);
+  void crear();
+  void imprime();
+  void redimensionar();
+}
+
 #endif //UNTITLED16_FUNCIONES_H
