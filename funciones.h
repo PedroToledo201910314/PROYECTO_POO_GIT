18269eecf112be0dcb48ac86ab56a3b63103d891
Joaquin-lab-poo1 <56448598+Joaquin-lab-poo1@users.noreply.github.com>
2019-10-19 16:04:24 -0500

    Update funciones.h

--- funciones.h
+++ funciones.h
@@ -6,8 +6,21 @@ using namespace std;
 
 class robot {
   private:
-  
+  numero* home=new numero(2);
+  string tipo_producto;
+  numero* ubicacion_actual=new numero(2);
+  numero numero_robot;
+  numero numero_intruccion;
+  string estado;
   public:
+  //Constructor por defecto
+  robot();
+  robot(numero* _home, string _tipo_producto, numero* _ubicacion, numero numero_robot, numero numero_instruccion, string _estado);
+  void dejar();
+  void recoger();
+  void mover();
+  bool colision();
+};
   
   
 #endif //UNTITLED16_FUNCIONES_H
