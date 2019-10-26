362b9f44182ebce4165cfc532fc2e324526ec7b9
AnaAF04 <55637924+AnaAF04@users.noreply.github.com>
2019-10-26 18:56:35 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,5 +11,29 @@ int main() {
   almacen a1;
   a1.crear();
   a1.imprime();
+  
+  int num_int=0, num_robots=0;
+  
+  //ROBOTS
+  cantidad_robots(num_robots);
+  vector<robot> robots(num_robots);
+  ingresar(robot,num_robots);
+  
+  //INSTRUCCIONES
+  num_instrucciones(num_ins);
+  vector<intruccion> instrucciones(num_ins);
+  pide_instrucciones(num_ins,instrucciones);
+  
+  //REDIMENSIONAR ALMACEN
+  string ans="";
+  cout<<endl<<"Desea redimensionar almacen (Si/No): ";
+  cin>>ans;
+  
+  for (int i = 0; i < ans.length(); i++) {ans[i] = toupper(ans[i]);}
+  if (ans=="SI"){
+    a1.redimesionar();
+    a1.imprime();
+  }
+  
   return 0;
 }
