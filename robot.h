87ba5a56f77ba3d28439c41e834499e0f515f308
AnaAF04 <55637924+AnaAF04@users.noreply.github.com>
2019-10-27 14:25:34 -0500

    Update robot.h

--- robot.h
+++ robot.h
@@ -1,1 +1,44 @@
+#ifndef ROBOT_H
+#define ROBOT_H
 
+#include <iostream>
+#include <vector>
+
+using numero=int;
+using letra=char;
+
+class robot{
+  private:
+    numero* _home=new numero[2];
+    string _tipo_producto;
+    numero* _ubicacion_actual=new numero[2];
+    numero _id_robot;
+    vector<instruccion> _instrucciones;
+    string _estado;
+  public:
+    //setter
+    void set_home(numero* home);
+    void set_tipo_producto(string tipo_producto);
+    void set_ubicacion_actual(numero* ubicación actual);
+    void set_id_robot(numero id_robot);
+    void set_instrucciones(vector<instruccion> instrucciones);
+    void set_estado(string estado);
+    
+    //getter
+    numero* get_home();
+    string get_tipo_producto();
+    numero* get_ubicacion_actual();
+    numero get_id_robot();
+    vector<instruccion> get_istrucciones();
+    string get_estado();
+
+    void dejar(string tipo_producto,slot posicion_slot, instruccion &la_instruccion);
+    void recoger();
+    numero** recorrido_ruta(numero*posicion_i,numero_posicion_f);
+    vector<vector<numero>>mover(vector<robot> robots,numero turno);
+    bool colision(vector<robot> robots);
+    ~robot();
+};
+  
+#endif
+  
