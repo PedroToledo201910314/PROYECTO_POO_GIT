b211264865fc4b18bca53e1e11c17f4e425a5e7c
Joaquin-lab-poo1 <56448598+Joaquin-lab-poo1@users.noreply.github.com>
2019-10-30 14:51:52 -0500

    Update instruccion.h

--- instruccion.h
+++ instruccion.h
@@ -1,1 +1,22 @@
+#ifndef INS_H
+#define INS_H
+#include "biblioteca.h"
 
+class instruccion{
+private:
+  numero _n_robot;
+  letra _operacion;
+  string _producto;
+  numero* _destino = new numero[2];
+public:
+  void set_n_robot(numero n_robot);
+  void set_operacion(letra operacion);
+  void set_destino(numero fila, numero columna);
+  void set_producto(string producto);
+  numero* get_destino();
+  letra get_operacion();
+  numero get_n_robot();
+  string get_producto(); 
+}
+
+#endif
