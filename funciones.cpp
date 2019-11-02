5ee0200c056ed62305273e34200b57a25c2b603a
Pedro Alfredo Toledo Garcia <55290916+PedroToledo201910314@users.noreply.github.com>
2019-11-01 20:43:04 -0500

    Update funciones.cpp

--- funciones.cpp
+++ funciones.cpp
@@ -1,8 +1,9 @@
 #include "funciones.h"
 #include "robot.h"
 #include "instruccion.h"
-#include <fstream>
-using namespace std;
+#include "slot.h"
+#include "almacen.h"
+#include "biblioteca.h"
 
 //Ingresa numero de robots
 void cantidad_robots(int & n_robots){
@@ -10,12 +11,17 @@ void cantidad_robots(int & n_robots){
   cout<<"Ingrese numero de robots: ";cin>>n_robots;}while(n_robots<3);
 }
 
-void ingresar(vector<robot>robots,numero num_robots){
-  cout<<endl;
+void ingresar(vector<robot> &robots, numero num_robots, vector<vector<slot>> slots){
+  cout<<endl; 
+  numero fila=0,columna=0;
   for (size_t i=0;i<num_robots;i++){
-    int* home = new int[2];
-    cout<<"Origen del robot #"<<i+1<<": ";cin>>home[i]>>home[i];
-    robots[i].set_home(home);
+    cout<<"Origen del robot #"<<i+1<<": "<<endl;
+    robots[i].set_id_robot(i+1);
+    cout<<"Fila: ";cin>>fila;
+    cout<<"Columna: ";cin>>columna;
+    (slots[fila][columna]).set_tipo_slot(true);
+    cout<<endl;
+    robots[i].set_home(fila,columna);
   }
 }
 
@@ -25,21 +31,25 @@ void num_instrucciones(int &num){
   cout<<"Ingrese numero de instrucciones: ";cin>>num;
 }
 
-void pide_instrucciones(int n_instrucciones, vector<instruccion> instrucciones){
+void pide_instrucciones(int n_instrucciones, vector<instruccion> &instrucciones, vector<robot> robots){
   for (size_t i=0;i<n_instrucciones;i++){
     cout<<endl<<"Instruccion "<<i+1<<": "<<endl;
+    cout<<"---------------"<<endl;
     int numero=0;
     cout<<"Numero de robot: ";cin>>numero;
     instrucciones[i].set_n_robot(numero);
     char op;
     cout<<"Operacion (Ingreso (I)/Retiro (R)): ";cin>>op;
     instrucciones[i].set_operacion(op);
-    int* des = new int[2];
-    cout<<"Destino (Fila, columna): ";cin>>des[0]>>des[1];
-    instrucciones[i].set_destino(des);
+    int fila = 0,columna = 0;
+    cout<<"Destino: "<<endl;
+    cout<<"Fila: ";cin>>fila;
+    cout<<"Columna: ";cin>>columna;
+    instrucciones[i].set_destino(fila, columna);
     string product="";
     cout<<"Producto: ";cin>>product;
     instrucciones[i].set_producto(product);
+    robots[numero-1].set_instrucciones(instruccion(instrucciones[i]));
   }
 }
 
@@ -53,4 +63,33 @@ void bienvenida(){
     cout<<texto<<endl;
   }
   archivo.close();
+}
+
+void realiza_instrucciones(vector<instruccion> instrucciones, vector<robot> robots, almacen &a1 ){
+  numero i=1;
+  cout<<endl<<"                   REPORTE DE INSTRUCCIONES                   "<<endl;
+  cout<<"__________________________________________________________"<<endl;
+  for (auto &ins:instrucciones){
+    cout<<endl<<"Reporte de instruccion "<<i<<":"<<endl;
+    cout<<"---------------------------"<<endl;
+    if (ins.get_operacion()=='I'|| ins.get_operacion()=='i'){
+        robots[ins.get_n_robot()-1].dejar(ins.get_producto(), ins, a1.get_matriz_slots(), robots, a1);
+      }
+    else if(ins.get_operacion()=='R'||(ins.get_operacion()=='r')){
+        robots[ins.get_n_robot()-1].recoger(ins.get_producto(), ins, a1.get_matriz_slots(), robots, a1);
+      }
+    i++;
+  }
+}
+
+void reporte_slots(almacen almacen_de_slots){
+  cout<<endl<<"                      REPORTE DE SLOTS                   "<<endl;
+  cout<<"__________________________________________________________"<<endl;
+  for (size_t i=0;i<almacen_de_slots.get_filas();i++){
+    for (size_t j=0;j<almacen_de_slots.get_columnas();j++){
+      cout<<"Slot "<<"["<<i<<"]["<<j<<"]:"<<endl;
+      cout<<"Tipo de producto: "<<almacen_de_slots.get_matriz_slots()[i][j].get_tipo_producto()<<endl;
+      cout<<"Cantidad: "<<almacen_de_slots.get_matriz_slots()[i][j].get_cantidad()<<endl;
+    }
+  }
 }
