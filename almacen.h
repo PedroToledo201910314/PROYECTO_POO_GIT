8710fa90530bcc2e42c3df05a98056556a1c2437
Pedro Alfredo Toledo Garcia <55290916+PedroToledo201910314@users.noreply.github.com>
2019-11-01 20:42:29 -0500

    Update almacen.h

--- almacen.h
+++ almacen.h
@@ -1,16 +1,10 @@
 #ifndef ALMACEN_H
 #define ALMACEN_H
-#include <iostream>
-#include <vector>
-#include <string>
-#include <iomanip>
-#include <map>
-#include <cmath>
-#include "slot.h"
-using namespace std;
-using numero = int;
-using letra = char;
-using arreglo=vector<char>;
+
+#include "slot.h" 
+#include "robot.h"  
+#include "biblioteca.h" 
+#include "instruccion.h" 
 
 
 class almacen{
@@ -21,17 +15,24 @@ class almacen{
   vector <arreglo> _matriz;
   vector<vector<slot>> _matriz_slots;
   public:
-  almacen();
-
   void set_filas(numero filas);
   void set_columnas(numero columnas);
   void set_capacidad(numero capacidad);
   void set_matriz(vector<arreglo> matriz);
-
+  
   vector<arreglo> get_matriz();
+  vector<vector<slot>> get_matriz_slots();
+  numero get_filas();
+  numero get_columnas();
 
   void crear();
   void imprime();
+  void llena_almacen(vector <robot> robots, numero num_robots);
+
+  void posiciones_finales(vector <robot> robots, numero num_robots, vector<instruccion> &instrucciones);
+
+  void dibujar(vector<vector<numero>> mover, numero n_robot); 
+
   void redimensionar();
 };
 
