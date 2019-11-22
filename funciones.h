2e1ac1abae05be3c47d40a025de5ff9e7e43c04d
Pedro Alfredo Toledo Garcia <55290916+PedroToledo201910314@users.noreply.github.com>
2019-11-22 11:13:52 -0500

    Update funciones.h

--- funciones.h
+++ funciones.h
@@ -1,27 +1,22 @@
-#ifndef PROYECTO_H
-#define PROYECTO_H
+#ifndef FUNCIONES_H
+#define FUNCIONES_H
 
-#include "funciones.h"
-#include "robot.h"
 #include "instruccion.h"
-#include "slot.h"
-#include "almacen.h"
+#include "robot.h"
 #include "biblioteca.h"
-
-vector<vector<letra>> instrucciones_robot (numero num_instrucciones,vector<instruccion> &instrucciones);
-
-void cantidad_robots(numero & n_robots);
-
-void ingresar(vector<robot> &robots, numero num_robots, vector<vector<slot>> slots);
-
-void num_instrucciones(numero &num);
-
-void pide_instrucciones(numero n_instrucciones, vector<instruccion> &instrucciones, vector<robot> robots);
-
-void realiza_instrucciones(vector<instruccion> instrucciones, vector<robot> robots, almacen &a1);
-
-void reporte_slots(almacen almacen_de_slots);
+#include "almacen.h"
 
 void bienvenida();
+void ingresar_coordenadas_ruta(robot_t* &robot1, instruccion_t* ins);
+
+void cantidad_robots(int & n_robots);
+void num_instrucciones(int &num);
+void setea_almacen (numero &filas, numero &columnas, numero &capacidad);
+vector<instruccion_t*> pide_instrucciones(numero n_instrucciones,almacen_t* almacen);
+void dibuja_ruta(numero columnas, numero filas, vector<arreglo> matriz);
+void reporte_slots(almacen_t* almacen_de_slots);
+void reporte(vector<instruccion_t*> instrucciones, almacen_t* a1, numero num_ins);
+
+void rutas(numero n_instrucciones,vector<instruccion_t*> instrucciones,numero filas_almacen, numero columnas_almacen, almacen_t* a1);
 
 #endif
