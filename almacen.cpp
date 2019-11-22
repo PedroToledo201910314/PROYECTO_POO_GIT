8b79770143f7cfa50b3d18be9d3c62c6f4663e02
Pedro Alfredo Toledo Garcia <55290916+PedroToledo201910314@users.noreply.github.com>
2019-11-22 11:12:59 -0500

    Update almacen.cpp

--- almacen.cpp
+++ almacen.cpp
@@ -1,68 +1,61 @@
 #include "almacen.h"
-#include "robot.h"
-#include "funciones.h"
 #include "biblioteca.h"
-#include "instruccion.h"
+#include "slot.h"
+#include "robot.h"
 
-void almacen::set_filas(numero filas){
-  _filas = filas;
-}
-
-void almacen::set_columnas(numero columnas){
-  _columnas = columnas;
-}
-
-void almacen::set_capacidad(numero capacidad){
-  _capacidad = capacidad;
-}
-
-void almacen::set_matriz(vector<arreglo> matriz){
-  _matriz = matriz;
-}
-  
-vector<arreglo> almacen::get_matriz(){
-  return _matriz;
-}
-
-vector<vector<slot>> almacen::get_matriz_slots(){
-  return _matriz_slots;
-}
-
-numero almacen::get_filas(){
-  return _filas;
-}
-
-numero almacen::get_columnas(){
-  return _columnas;
-}
-
-void almacen::crear(){
-  cout<<"Ingrese filas del almacen: ";cin>>_filas;
-  cout<<"Ingrese columnas del almacen: ";cin>>_columnas;
-  cout<<"Ingrese capacidad: ";cin>>_capacidad;
-  _matriz_slots = vector<vector<slot>> (_filas, vector <slot>(_columnas));
-  _matriz = vector<arreglo>(_filas, arreglo(_columnas,' '));
+almacen_t::almacen_t(numero _filas, numero _columnas, numero _capacidad):filas(_filas),columnas(_columnas),capacidad(_capacidad){
+  vector<slot_t*> vec_slots(_columnas);
+  for (size_t i=0;i<_filas;i++){
+    matriz_slots.push_back(vec_slots);
+  }
   for (size_t i=0;i<_filas;i++){
     for(size_t j=0;j<_columnas;j++){
-      (_matriz_slots[i][j]).set_capacidad(_capacidad);
+      slot_t* s = new slot_t(i,j,capacidad);
+      matriz_slots[i][j]=s;
     }
   }
+  vector<arreglo> matriz_temp(_filas, arreglo(_columnas,' '));
+  matriz = matriz_temp;
 }
 
-void almacen::imprime(){
+vector<arreglo> almacen_t::get_matriz(){
+  return matriz;
+}
+
+vector<vector<slot_t*>> almacen_t::get_matriz_slots(){
+  return matriz_slots;
+}
+
+numero almacen_t::get_filas(){
+  return filas;
+}
+
+numero almacen_t::get_columnas(){
+  return columnas;
+}
+
+numero almacen_t::get_capacidad(){
+  return capacidad;
+}
+
+vector <robot_t*> almacen_t::get_robots(){
+  return robots;
+}
+
+void almacen_t::imprime(){
   cout<<endl;
   cout<<"  ";
-  for(size_t z=0;z<_columnas;z++){
+  for(size_t z=0;z<columnas;z++){
     cout<<setw(4)<<z;
   } cout<<endl;
   cout<<"   ";
-  for(size_t z=0;z<_columnas;z++){
+  for(size_t z=0;z<columnas;z++){
     cout<<setw(4)<<"___";
   }
   cout<<endl;
-  for(size_t i=0;i<_filas;i++){
+  for(size_t i=0;i<filas;i++){
     cout<<"   ";
-    for(size_t j=0;j<_columnas;j++){
+    for(size_t j=0;j<columnas;j++){
       cout<<"|"<<setw(4);
     }
     cout<<"|"<<endl;
@@ -70,13 +63,13 @@ void almacen::imprime(){
     if(i<10){
       cout << ' ';
     }
-    for(size_t j=0;j<_columnas;j++){
-      _matriz[i][j];
-      cout<<"|"<<setw(2)<<_matriz[i][j]<<setw(2);
+    for(size_t j=0;j<columnas;j++){
+      matriz[i][j];
+      cout<<"|"<<setw(2)<<matriz[i][j]<<setw(2);
     }
     cout<<"|"<<endl;
     cout<<"   ";
-    for(size_t j=0;j<_columnas;j++){
+    for(size_t j=0;j<columnas;j++){
       cout<<"|___";
     }
     cout<<"|"<<endl;
@@ -84,30 +77,22 @@ void almacen::imprime(){
   cout<<endl;
 }
 
-void almacen::redimensionar(){
-  cout<<endl<<"Ingrese nueva cantidad de filas del almacen: ";cin>>_filas;
-  cout<<"Ingrese nueva cantidad de columnas del almacen: ";cin>>_columnas;
-  _matriz = vector<arreglo>(_filas, arreglo(_columnas,' '));
-  _matriz_slots = vector<vector<slot>> (_filas, vector <slot>(_columnas));
+void almacen_t::set_robots(numero cantidad_robots){
+  numero fila=0,columna=0;
+  for (size_t i=0;i<cantidad_robots;i++){
+    cout<<endl<<"Origen del robot #"<<i+1<<": "<<endl;
+    cout<<"Fila: ";cin>>fila;
+    cout<<"Columna: ";cin>>columna;
+    robot_t* robot = new robot_t(fila,columna,i+1);
+    robots.push_back(robot);
+    matriz_slots[fila][columna]->set_tipo_slot(true);
+  }
 }
 
-void almacen::llena_almacen (vector<robot> robots, numero num_robots){
+void almacen_t::llena_almacen (numero num_robots){
   for (size_t k=0;k<num_robots;k++){
     char number='0'+k+1;
-    _matriz[(robots[k].get_home())[0]][(robots[k].get_home())[1]]=number;
+    matriz[robots[k]->get_homex()][robots[k]->get_homey()]=number;
   }
 }
 
-void almacen::dibujar(vector<vector<numero>> mover, numero n_robot){
-  for (size_t k=0;k<mover.size();k++){
-    char number='0'+n_robot;
-    _matriz[(mover[k][0])][(mover[k][1])]=number ;}
-}
-
-void almacen::posiciones_finales (vector<robot> robots, numero num_robots, vector<instruccion> &instrucciones){
-  for (size_t k=0;k<instrucciones.size();k++){
-    numero robot_asignado = instrucciones[k].get_n_robot();
-    robots[robot_asignado-1].set_ubicacion_actual((instrucciones[k].get_destino())[0],(instrucciones[k].get_destino())[1]);
-    _matriz[(robots[robot_asignado-1].get_ubicacion_actual())[0]][(robots[robot_asignado-1].get_ubicacion_actual())[1]]='+';
-  }
-}
