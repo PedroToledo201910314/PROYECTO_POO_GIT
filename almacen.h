bc82edd907ccccf56c270a61b0c9c41df3e391ce
Pedro Alfredo Toledo Garcia <55290916+PedroToledo201910314@users.noreply.github.com>
2019-11-22 11:10:17 -0500

    Update almacen.h

--- almacen.h
+++ almacen.h
@@ -1,39 +1,28 @@
 #ifndef ALMACEN_H
 #define ALMACEN_H
 
-#include "slot.h" 
-#include "robot.h"  
-#include "biblioteca.h" 
-#include "instruccion.h" 
+#include "robot.h"
+#include "slot.h"
 
-
-class almacen{
+class almacen_t{
   private:
-  numero _filas;
-  numero _columnas;
-  numero _capacidad;
-  vector <arreglo> _matriz;
-  vector<vector<slot>> _matriz_slots;
+    numero filas;
+    numero columnas;
+    numero capacidad;
+    vector<arreglo> matriz;
+    vector<vector<slot_t*>> matriz_slots;
+    vector<robot_t*> robots;
   public:
-  void set_filas(numero filas);
-  void set_columnas(numero columnas);
-  void set_capacidad(numero capacidad);
-  void set_matriz(vector<arreglo> matriz);
-  
-  vector<arreglo> get_matriz();
-  vector<vector<slot>> get_matriz_slots();
-  numero get_filas();
-  numero get_columnas();
-
-  void crear();
-  void imprime();
-  void llena_almacen(vector <robot> robots, numero num_robots);
-
-  void posiciones_finales(vector <robot> robots, numero num_robots, vector<instruccion> &instrucciones);
-
-  void dibujar(vector<vector<numero>> mover, numero n_robot); 
-
-  void redimensionar();
+    almacen_t(numero _filas, numero _columnas, numero _capacidad);
+    vector<arreglo> get_matriz();
+    vector<vector<slot_t*>> get_matriz_slots();
+    numero get_filas();
+    numero get_columnas();
+    numero get_capacidad();
+    vector <robot_t*> get_robots();
+    void set_robots(numero cantidad_robots);
+    void imprime();
+    void llena_almacen(numero num_robots);
 };
 
 #endif
