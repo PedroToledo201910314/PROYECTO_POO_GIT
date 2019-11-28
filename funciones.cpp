37f966cb97f35702347157818aa11f7fe6de466b
AnaAF04 <55637924+AnaAF04@users.noreply.github.com>
2019-11-28 18:58:51 -0500

    Update funciones.cpp

--- funciones.cpp
+++ funciones.cpp
@@ -248,4 +248,131 @@ void reporte(vector<instruccion_t*> instrucciones, almacen_t* a1, numero num_ins
   }
 }
 
-//De rutas en adelante
+void rutas(numero n_instrucciones,vector<instruccion_t*> instrucciones,numero filas_almacen, numero columnas_almacen, almacen_t* a1){
+  cout<<endl<<endl<<"                   RUTAS PARA CADA INSTRUCCION                  "<<endl<<"_________________________________________________________"<<endl;
+//vector<columna> costruir_ruta_p(coordenada xi, coordenada yi, coordenada xf, coordenada yf)
+ofstream rutas ("rutas.txt");
+  for (size_t k=0;k<n_instrucciones;k++){
+    if(instrucciones[k]->get_estado()==true){
+      auto robot=a1->get_robots()[instrucciones[k]->get_n_robot()-1];
+      vector <columna> v;
+      if(tolower(instrucciones[k]->get_operacion())=='i'){
+        
+        vector<columna> v1;
+        
+        if(robot->get_ubicacion_actualx()!=robot->get_homex() && robot->get_ubicacion_actualy()!=robot->get_homey()){
+          
+          v1=costruir_ruta_p(robot->get_ubicacion_actualx(), robot->get_ubicacion_actualy(), robot->get_homex(),robot->get_homey());
+
+        }
+
+        vector<columna> v2=costruir_ruta_p(robot->get_homex(),robot->get_homey(),instrucciones[k]->get_destinox(),instrucciones[k]->get_destinoy());
+
+        for (auto& i:v1)
+          v.push_back(i);
+        for (auto& j:v2)
+          v.push_back(j);
+
+      }
+      else{
+        vector<columna> v1=costruir_ruta_p(robot->get_ubicacion_actualx(),robot->get_ubicacion_actualy(),instrucciones[k]->get_destinox(),instrucciones[k]->get_destinoy());
+        
+        vector<columna> v2=costruir_ruta_p(instrucciones[k]->get_destinox(), instrucciones[k]->get_destinoy(), robot->get_homex(),robot->get_homey()); 
+      
+      for (auto& i:v1)
+          v.push_back(i);
+        for (auto& j:v2)
+          v.push_back(j);
+      }
+        //Importante
+      for (auto &i:v){
+        robot->_ruta.push_back(i);
+        }
+      robot->_rutas_dibujar.push_back(v);
+
+      vector<arreglo> dibujo_ruta (filas_almacen,arreglo(columnas_almacen,' '));
+      
+      dibujo_ruta[a1->get_robots()[instrucciones[k]->get_n_robot()-1]->get_ubicacion_actualx()][a1->get_robots()[instrucciones[k]->get_n_robot()-1]->get_ubicacion_actualy()]='*';
+
+      rutas<<"Instruccion "<<k+1<<":"<<endl;
+      rutas<<"Numero de robot: "<<instrucciones[k]->get_n_robot()<<endl;
+      for (int i = 0; i < v.size(); ++i){
+        vector<coordenada> posicion(2);
+        int p=0;
+        for (int j = 0; j < v[i].size(); ++j){
+          rutas<<v[i][j]<<" ";
+          posicion[p]=v[i][j];
+          p++;
+        }
+        rutas<<endl;
+        char number='a'+i;
+        dibujo_ruta[posicion[0]][posicion[1]]=number;
+
+        a1->get_robots()[instrucciones[k]->get_n_robot()-1]->set_ubicacion_actual(posicion[0],posicion[1]);
+      }
+
+      cout<<endl<<"Ruta de instruccion "<<k+1<<":"<<endl;
+      cout<<"_______________________"<<endl;
+      dibuja_ruta(columnas_almacen, filas_almacen, dibujo_ruta);
+    }
+  rutas<<endl;
+  }
+}
+
+void genera_archivos_slots(almacen_t* almacen_de_slots){
+  ofstream slots("slots.txt");
+  for (size_t i=0;i<almacen_de_slots->get_filas();i++){
+    for (size_t j=0;j<almacen_de_slots->get_columnas();j++){
+      if(almacen_de_slots->get_matriz_slots()[i][j]->get_tipo_slot()==false){
+      slots<<endl<<"Slot "<<"["<<i<<"]["<<j<<"]:"<<endl;
+      slots<<"Tipo de producto: "<<almacen_de_slots->get_matriz_slots()[i][j]->get_tipo_producto()<<endl;
+      slots<<"Cantidad: "<<almacen_de_slots->get_matriz_slots()[i][j]->get_cantidad()<<endl;
+      }
+      else{
+      slots<<endl<<endl<<"Slot "<<"["<<i<<"]["<<j<<"]:"<<endl;
+      slots<<"Es un home de un robot.";
+      }
+    }
+  }
+}
+
+void logo(){
+  ifstream archivo;
+  archivo.open("logo.txt");
+  string texto;
+  while(!archivo.eof()){
+    getline(archivo,texto);
+    cout<<texto<<endl;
+  }
+  archivo.close();
+
+  char x = '0';
+  cout<<"Ingrese cualquier caracter para continuar..."<<endl;
+  do{
+    cin>>x;
+  }while(x == '0');
+  limpiar();
+}
+
+void limpiar(){
+  cout<<"\033[2J\033[0;0H";
+}
+
+void menu(vector<instruccion_t*> instrucciones, almacen_t* a1, numero n_instrucciones, numero filas_almacen, numero columnas_almacen){
+  numero opcion;
+  cout<<endl<<endl<<endl<<"MENÚ:"<<endl<<endl<<"1. Reporte de instrucciones."<<endl<<"2. Rutas de robots."<<endl<<"3. Reporte de slots";
+  cout<<endl<<endl<<"Ingrese opción: ";cin>>opcion;
+  switch(opcion){
+    case 1:
+      reporte(instrucciones,a1,n_instrucciones); //Reporte
+      break;
+    case 2:
+      rutas(n_instrucciones, instrucciones, filas_almacen, columnas_almacen, a1);
+      break;
+    case 3:
+      reporte_slots(a1);
+      break;
+    default:
+      cout<<endl<<"Opción no válida.";
+  } //Menu
+}
