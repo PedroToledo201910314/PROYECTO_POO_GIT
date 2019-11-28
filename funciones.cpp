56c448ca1cba4f26c6548e37ead7861f13aad3a8
AnaAF04 <55637924+AnaAF04@users.noreply.github.com>
2019-11-27 22:40:24 -0500

    Update funciones.cpp

--- funciones.cpp
+++ funciones.cpp
@@ -5,36 +5,34 @@
 #include "instruccion.h"
 #include "almacen.h"
 
-void ingresar_coordenadas_ruta(robot_t* &robot1, instruccion_t* ins){
-  coordenada ubicx=robot1->get_ubicacion_actualx();
-  coordenada ubicy=robot1->get_ubicacion_actualy();
-  if(ubicx!=ins->get_destinox()||ubicy!=ins->get_destinoy()){
-    while(ubicx!=ins->get_destinox()||ubicy!=ins->get_destinoy()){
+vector<columna> costruir_ruta_p(coordenada xi, coordenada yi, coordenada xf, coordenada yf){
+  vector<columna> ruta_p;
+  coordenada ubicx=xi;
+  coordenada ubicy=yi;
+  if(ubicx!=xf||ubicy!=yf){
+    while(ubicx!=xf||ubicy!=yf){
       int nx=0;
       int ny=0;
-      if(ubicx<ins->get_destinox()){
+      if(ubicx<xf){
         nx=1;
-        if(ubicy<ins->get_destinoy())
+        if(ubicy<yf)
           ny=1;
-        else if (ubicy==ins->get_destinoy())
+        else if (ubicy==yf)
           ny=0;
         else
           ny=-1;
       }
-      else if(ubicx==ins->get_destinox()){
-        if(ubicy<ins->get_destinoy()){
-          ny=1;}
-        else if (ubicy==ins->get_destinoy()){
-          ny=0;
-        }
-        else{
-          ny=-1;}
+      else if(ubicx==xf){
+        if(ubicy<yf)
+          ny=1;
+        else
+          ny=-1;
       }
       else{
         nx=-1;
-        if(ubicy<ins->get_destinoy())
+        if(ubicy<yf)
           ny=1;
-        else if (ubicy==ins->get_destinoy())
+        else if (ubicy==yf)
           ny=0;
         else
           ny=-1;
@@ -42,28 +40,29 @@ void ingresar_coordenadas_ruta(robot_t* &robot1, instruccion_t* ins){
       coordenada respaldox=ubicx;
       coordenada respaldoy=ubicy;
       ubicx=ubicx+nx;
-      columna coordenadas_ruta1 = {ubicx, ubicy};
+      columna coordenadas_ruta1= {ubicx, ubicy};
       ubicy=ubicy+ny;
-      columna coordenadas_ruta2 = {ubicx, ubicy};
+      columna coordenadas_ruta2= {ubicx, ubicy};
       if(coordenadas_ruta1[0]==respaldox&&coordenadas_ruta1[1]==respaldoy){
-        robot1->_ruta.push_back(coordenadas_ruta2);
+        ruta_p.push_back(coordenadas_ruta2);
       }
       else{
         if(coordenadas_ruta1[0]==coordenadas_ruta2[0]&&coordenadas_ruta1[1]==coordenadas_ruta2[1]){
-          robot1->_ruta.push_back(coordenadas_ruta2);
+          ruta_p.push_back(coordenadas_ruta2);
         }
         else{
-          robot1->_ruta.push_back(coordenadas_ruta1);
-          robot1->_ruta.push_back(coordenadas_ruta2);
+          ruta_p.push_back(coordenadas_ruta1);
+          ruta_p.push_back(coordenadas_ruta2);
         }
       }
     }
   }
+  return ruta_p;
 }
 
 void bienvenida(){
   ifstream archivo;
-  archivo.open("inicio.txt",ios::in);
+  archivo.open("inicio.txt");
   string texto;
   while(!archivo.eof()){
     getline(archivo,texto);
@@ -105,6 +104,9 @@ vector<instruccion_t*> pide_instrucciones (numero n_instrucciones,almacen_t* alm
     string product="";
     if(tolower(op)=='i'){
       cout<<"Producto: ";cin>>product;
+    }
+    else{
+      product=almacen->get_matriz_slots()[fila][columna]->get_tipo_producto();
     }
 
     instruccion_t* ins = new instruccion_t(numero,op,product,fila,columna);
@@ -155,24 +157,34 @@ void reporte_slots(almacen_t* almacen_de_slots){
   cout<<"__________________________________________________________"<<endl;
   numero x,y;
   cout<<endl<<"Si desea finalizar proceso ingrese -1 en filas.";
+
+  genera_archivos_slots(almacen_de_slots);
+
   do{
     cout<<endl<<endl<<"Ingrese fila de slot: ";cin>>x;
     if(x==-1){
       break;
     }
     cout<<"Ingrese columna de slot: ";cin>>y;
-    cout<<endl<<endl<<"Slot "<<"["<<x<<"]["<<y<<"]:"<<endl;
-    cout<<"Tipo de producto: "<<almacen_de_slots->get_matriz_slots()[x][y]->get_tipo_producto()<<endl;
-    cout<<"Cantidad: "<<almacen_de_slots->get_matriz_slots()[x][y]->get_cantidad()<<endl;
-    cout<<"Estado: ";
-    if(almacen_de_slots->get_matriz_slots()[x][y]->get_estado()=='E'){
+    if(almacen_de_slots->get_matriz_slots()[x][y]->get_tipo_slot()==false){
+
+      cout<<endl<<endl<<"Slot "<<"["<<x<<"]["<<y<<"]:"<<endl;
+      cout<<"Tipo de producto: "<<almacen_de_slots->get_matriz_slots()[x][y]->get_tipo_producto()<<endl;
+      cout<<"Cantidad: "<<almacen_de_slots->get_matriz_slots()[x][y]->get_cantidad()<<endl;
+      cout<<"Estado: ";
+      if(almacen_de_slots->get_matriz_slots()[x][y]->get_estado()=='E'){
         cout<<"Vacío";
-    }
-    else if(almacen_de_slots->get_matriz_slots()[x][y]->get_estado()=='L'){
+      }
+      else if(almacen_de_slots->get_matriz_slots()[x][y]->get_estado()=='L'){
         cout<<"Libre";
+      }
+      else{
+        cout<<"Lleno";
+      }
     }
     else{
-        cout<<"Lleno";
+      cout<<endl<<endl<<"Slot "<<"["<<x<<"]["<<y<<"]:"<<endl;
+      cout<<"Es un home de un robot.";
     }
   }while(true);
 }
@@ -184,87 +196,53 @@ void reporte(vector<instruccion_t*> instrucciones, almacen_t* a1, numero num_ins
     cout<<endl<<endl<<"Reporte de instruccion "<<i+1<<":"<<endl;
     cout<<"---------------------------"<<endl;
     if(a1->get_matriz_slots()[instrucciones[i]->get_destinox()][instrucciones[i]->get_destinoy()]->get_tipo_slot() == true){
-          cout<<"Este es el home de un robot.";
+          cout<<"Este es el home de un robot."<<endl;
           instrucciones[i]->set_estado(false);
     }
     else{
       if(tolower(instrucciones[i]->get_operacion())=='i'){
-        if (a1->get_matriz_slots()[instrucciones[i]->get_destinox()][instrucciones[i]->get_destinoy()]->get_estado()=='E'){
-          cout<<"No hubo problemas.";
-          instrucciones[i]->set_estado(true);
+        if(a1->get_matriz_slots()[instrucciones[i]->get_destinox()][instrucciones[i]->get_destinoy()]->get_cantidad()==a1->get_matriz_slots()[instrucciones[i]->get_destinox()][instrucciones[i]->get_destinoy()]->get_capacidad()){
+            
+            cout<<"Slot lleno";
+            instrucciones[i]->set_estado(false);
         }
-        else if(a1->get_matriz_slots()[instrucciones[i]->get_destinox()][instrucciones[i]->get_destinoy()]->get_estado()=='L'){
-            if(instrucciones[i]->get_producto()==a1->get_matriz_slots()[instrucciones[i]->get_destinox()][instrucciones[i]->get_destinoy()]->get_tipo_producto()){
-              cout<<"No hubo problemas.";
-              instrucciones[i]->set_estado(true);
-            }
-            else{
-              cout<<"Este slot contiene un producto diferente al cual desea ingresar.";
-              instrucciones[i]->set_estado(false);
-            }
+
+        else if (a1->get_matriz_slots()[instrucciones[i]->get_destinox()][instrucciones[i]->get_destinoy()]->get_cantidad()== 0){
+
+            instrucciones[i]->set_estado(true);
+
+            a1->get_matriz_slots()[instrucciones[i]->get_destinox()][instrucciones[i]->get_destinoy()]->adicionar(instrucciones[i]->get_producto());
         }
+
         else{
-          cout<<"Este slot esta lleno.";
-          instrucciones[i]->set_estado(false);
+          instrucciones[i]->set_estado(true);
+
+          a1->get_matriz_slots()[instrucciones[i]->get_destinox()][instrucciones[i]->get_destinoy()]->adicionar(instrucciones[i]->get_producto());
         }
       }
+
       else{
-        if(a1->get_matriz_slots()[instrucciones[i]->get_destinox()][instrucciones[i]->get_destinoy()]->get_estado()=='E'){
-          cout<<"Este slot está vacío.";
+        if(a1->get_matriz_slots()[instrucciones[i]->get_destinox()][instrucciones[i]->get_destinoy()]->get_cantidad()==0){
+          cout<<"Slot vacío.";
           instrucciones[i]->set_estado(false);
         }
-        else if(a1->get_matriz_slots()[instrucciones[i]->get_destinox()][instrucciones[i]->get_destinoy()]->get_estado()=='L'){
+
+        else{ 
           cout<<"No hubo problemas.";
           instrucciones[i]->set_estado(true);
-        }
-        else{
-          cout<<"No hubo problemas.";
-          instrucciones[i]->set_estado(true);
+
+          a1->get_matriz_slots()[instrucciones[i]->get_destinox()][instrucciones[i]->get_destinoy()]->retirar();
         } 
-      } 
-    cout<<endl;
+
+      }
     }
+    cout<<endl<<"Estado: ";
+    if(instrucciones[i]->get_estado()==true){
+      cout<<"Activo.";
+    }
+    else {cout<<"Inactivo.";}
   }
 }
 
-void rutas(numero n_instrucciones,vector<instruccion_t*> instrucciones,numero filas_almacen, numero columnas_almacen, almacen_t* a1){
-  cout<<endl<<endl<<"                   RUTAS PARA CADA INSTRUCCION                  "<<endl<<"_________________________________________________________"<<endl;
 
-  for (size_t k=0;k<n_instrucciones;k++){
-    if(instrucciones[k]->get_estado()==true){
-      ingresar_coordenadas_ruta(a1->get_robots()[instrucciones[k]->get_n_robot()-1],instrucciones[k]);
-      
-      vector<columna> v=a1->get_robots()[instrucciones[k]->get_n_robot()-1]->_ruta;
-
-      vector<arreglo> dibujo_ruta (filas_almacen,arreglo(columnas_almacen,' '));
-      
-      dibujo_ruta[a1->get_robots()[instrucciones[k]->get_n_robot()-1]->get_ubicacion_actualx()][a1->get_robots()[instrucciones[k]->get_n_robot()-1]->get_ubicacion_actualy()]='*';
-    
-      for (int i = 0; i < v.size(); ++i){
-        vector<coordenada> posicion(2);
-        int p=0;
-        for (int j = 0; j < v[i].size(); ++j){
-          posicion[p]=v[i][j];
-          p++;
-        }
-        char number='0'+i+1;
-        dibujo_ruta[posicion[0]][posicion[1]]=number;
-
-        a1->get_robots()[instrucciones[k]->get_n_robot()-1]->set_ubicacion_actual(posicion[0],posicion[1]);
-      }
-
-      cout<<endl<<"Ruta de instruccion "<<k+1<<":"<<endl;
-      cout<<"_______________________"<<endl;
-      dibuja_ruta(columnas_almacen, filas_almacen, dibujo_ruta);
-
-      if(tolower(instrucciones[k]->get_operacion())=='i'){
-        
-        a1->get_matriz_slots()[instrucciones[k]->get_destinox()][instrucciones[k]->get_destinoy()]->adicionar(instrucciones[k]->get_producto());
-      }
-      
-      else{
-        a1->get_matriz_slots()[instrucciones[k]->get_destinox()][instrucciones[k]->get_destinoy()]->retirar(instrucciones[k]->get_producto());
-      }
-    }
-  }
-}
+//De rutas en adelante
