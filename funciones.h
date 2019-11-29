dd291ef598ebc01b3b0fa6983c6777c04a619d6d
Pedro Alfredo Toledo Garcia <55290916+PedroToledo201910314@users.noreply.github.com>
2019-11-28 19:06:48 -0500

    Update funciones.h

--- funciones.h
+++ funciones.h
@@ -7,9 +7,9 @@
 #include "almacen.h"
 
 void bienvenida();
-void ingresar_coordenadas_ruta(robot_t* &robot1, instruccion_t* ins);
+void ingresar_coordenadas_ruta(robot_t* robot1, instruccion_t* ins);
 
-void cantidad_robots(int & n_robots);
+void cantidad_robots(int & n_robots, numero f, numero c);
 void num_instrucciones(int &num);
 void setea_almacen (numero &filas, numero &columnas, numero &capacidad);
 vector<instruccion_t*> pide_instrucciones(numero n_instrucciones,almacen_t* almacen);
@@ -18,5 +18,13 @@ void reporte_slots(almacen_t* almacen_de_slots);
 void reporte(vector<instruccion_t*> instrucciones, almacen_t* a1, numero num_ins);
 
 void rutas(numero n_instrucciones,vector<instruccion_t*> instrucciones,numero filas_almacen, numero columnas_almacen, almacen_t* a1);
+
+void genera_archivos_slots(almacen_t* almacen_de_slots);
+
+void logo();
+
+void limpiar();
+
+void menu(vector<instruccion_t*> instrucciones, almacen_t* a1, numero n_instrucciones, numero filas_almacen, numero columnas_almacen);
 
 #endif
